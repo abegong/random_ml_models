fd9f07b7a2878862a6b86a81c2d5eb9170db021e
Abe <abegong@gmail.com>
2021-02-21 12:26:18 -0700

    Add setup-.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "random_ml_models",
+    version = "0.0.4",
+    author = "Abe Gong",
+    author_email = "abegong@gmail.com",
+    description = "Generate random ML models",
+    license = "Apache 2.0",
+    keywords = "",
+    url = "",
+    packages=['random_ml_models', 'tests'],
+    long_description="",
+    # classifiers=[],
+)
